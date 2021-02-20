1ed9c49e9fcafda56a0a0834787c9005a5dd97bb
naveenoo9 <naveenkeshav9@gmail.com>
2021-02-21 00:34:35 +0530

    Uploading My_Employee_Manager

---
+++ src/main/java/com/employeemanager/myapplications/MyEmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanager.myapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEmployeeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeemanager/myapplications/MyEmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanager.myapplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEmployeeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
